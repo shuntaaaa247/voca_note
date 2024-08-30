9f4d5b3edb164be952201072a5961236f1bf70bd
shuntaaaa247 <juntairenzu3@gmail.com>
2024-08-30 11:42:21 +0900

    エンドポイントの追加

---
+++ backend/prisma/migrations/20240829150532_/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" TEXT NOT NULL,
+    "word" TEXT NOT NULL,
+    "meaning" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/src/routes/category.ts
+++ backend/src/routes/category.ts
@@ -1,12 +1,36 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { z } from "zod";
+import { verifyToken } from "../middlewares/verifyToken";
+import { CategorySchema, Category } from "../../generated/zod";
 
 
-const categoryRouter: Router = Router();
+export const categoryRouter: Router = Router();
+const prisma = new PrismaClient();
 
-categoryRouter.post("/", (req: Request, res: Response, next: NextFunction) => {
+categoryRouter.use(verifyToken);
+
+categoryRouter.post("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
-
-  } catch {
-    
+    const categoryData = CategorySchema.omit({ id: true }).parse({categoryName: req.body.categoryName, userId: req.decoded.id});
+    const category: Category = await prisma.category.create({
+      data: categoryData
+    });
+    return res.status(200).json({ message: "作成完了", category })
+  } catch(e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return res.status(400).json({ message: "そのカテゴリー名はすでに使用されています。"})
+      }
+    }
+    if (e instanceof z.ZodError) {
+      return res.status(400).json({ message: `${e.issues[0].path}: ${e.issues[0].message}`})
+    }
+    console.log(e)
+    return res.status(500).json({ message: "予期せぬエラーが発生しました。" })
   }
+})
+
+categoryRouter.all("/items", (req: Request, res: Response, next: NextFunction) => {
+  next("route");
 })
