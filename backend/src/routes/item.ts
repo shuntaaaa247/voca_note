d7cdd6d6001869c344f823b458b942dfabdf5e88
shuntaaaa247 <juntairenzu3@gmail.com>
2024-08-31 20:04:31 +0900

    パスの追加

--- backend/src/routes/item.ts
+++ backend/src/routes/item.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 export const itemRouter = Router();
 
 // categoryルータからここに処理が渡される。
-itemRouter.post("/", async (req: Request, res: Response, next: NextFunction) => {
+itemRouter.post("/items", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category: Category | null = await prisma.category.findUnique({
       where: {
@@ -32,5 +32,40 @@ itemRouter.post("/", async (req: Request, res: Response, next: NextFunction) => 
       return res.status(400).json({ message: `${e.issues[0].path}: ${e.issues[0].message}`})
     }
     return res.json(500).json({ message: "予期せぬエラーが発生しました.。" })
+  }
+});
+
+itemRouter.get("/:categoryId/items", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log("ああああ")
+    console.log(req.params.categoryId);
+    const category: Category | null = await prisma.category.findUnique({
+      where: {
+        id: req.params.categoryId
+        // categoryName_userId: {
+        //   userId: req.decoded.id,
+        //   categoryName: req.params.categoryId
+        // }
+      }
+    })
+    
+    if (!category) {
+      return res.status(404).json({ message: "カテゴリーが見つかりませんでした。" })
+    }
+
+    if (category.userId !== req.decoded.id) {
+      return res.status(403).json({ message: "権限がありません。" })
+    }
+
+    const items: Item[] = await prisma.item.findMany({
+      where: {
+        categoryId: req.params.categoryId,
+      }
+    })
+
+    return res.status(200).json({ message: "取得完了", items });
+  } catch(e) {
+    console.log(e);
+    return res.status(500).json({ message: "予期せぬエラーが発生しました。" })
   }
 })
