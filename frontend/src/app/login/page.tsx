91721fcd0d388e777a4b1718939aa2c1739da9ac
shuntaaaa247 <juntairenzu3@gmail.com>
2024-09-22 23:25:36 +0900

    バリデーション表示の追加

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { useRouter } from "next/navigation"
 import { useCookies } from 'next-client-cookies';
 import { User } from "../../../../backend/generated/zod"
@@ -10,33 +10,48 @@ const Login = () => {
   const cookies = useCookies();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("")
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setErrorMessage("");
+      const email: String | undefined = emailRef.current?.value;
+      const password: String | undefined = passwordRef.current?.value
 
-    const email: String | undefined = emailRef.current?.value;
-    const password: String | undefined = passwordRef.current?.value
-
-    const res = await fetch("http://localhost:5000/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password
+      const res = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
       })
-    })
 
-    const resJson = await res.json();
-    const user: User = resJson.user;
-    const token: string = resJson.token;
+      const resJson = await res.json();
 
-    console.log({user, token})
+      if (res.ok) {
+        const user: User = resJson.user;
+        const token: string = resJson.token;
 
-    cookies.set("token", token)
-    cookies.set("userId", user.id)
-    router.push("/");
+        cookies.set("token", token)
+        cookies.set("userId", user.id)
+        router.push("/");
+      } else if (res.status === 401) {
+        setErrorMessage(resJson.message);
+      } else {
+        return(
+          <div>500 Server Error</div>
+        )
+      }
+    } catch (e) {
+      console.log(e)
+      return(
+        <div>500 Server Error</div>
+      )
+    }
   }
 
   return(
@@ -54,9 +69,6 @@ const Login = () => {
           <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full"></span>
           <input ref={emailRef} type="email" placeholder="voca@note.com" className="bg-slate-50 text-slate-700 pl-2 pt-2 w-full focus:outline-none"/>
         </div>
-        {/* <div className="border-t flex py-1.5 mx-1">
-          <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full "></span>
-        </div> */}
         <div className="border-t flex mx-1">
           <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full"></span>
           <label className="pl-2 pt-2">パスワード</label>
@@ -65,14 +77,18 @@ const Login = () => {
           <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full"></span>
           <input ref={passwordRef} type="password" placeholder="パスワード" className="bg-slate-50 pl-2 pt-2 w-full focus:outline-none"/>
         </div>
-        {/* <div className="border-t flex py-1.5 mx-1">
-          <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full "></span>
-        </div> */}
         <div className="border-t flex mx-1">
           <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full "></span>
           {/* <button className="bg-blue-600 text-lg text-white rounded-lg hover:bg-blue-500 px-2 my-0.5 mx-auto">Login</button> */}
           <button type="submit" className="border-blue-500 border-2 text-blue-500 text-lg px-2 my-0.5 mx-auto hover:bg-blue-500 hover:text-white">Login</button>
         </div>
+        { errorMessage 
+        ? <div className="border-t flex mx-1">
+            <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full "></span>
+            <p className="mx-auto text-red-500">{errorMessage}</p>
+          </div>
+        : <></>
+        }
         <div className="border-t border-b flex mx-1">
           <span className="w-5 h-5 ml-1 my-auto shadow-inner bg-slate-200 rounded-full"></span>
           <Link href={"http://localhost:3000/signup"} className="pl-2 pt-2 text-blue-500 text-sm mx-auto">アカウントをお持ちでない方</Link>
