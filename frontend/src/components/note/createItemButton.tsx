a48d2e0e2c02206cc2fb7dfac46f18ba3a41666a
shuntaaaa247 <juntairenzu3@gmail.com>
2025-02-04 14:10:16 +0900

    アイテム作成モーダルの本番用UIの作成

--- frontend/src/components/note/createItemButton.tsx
+++ frontend/src/components/note/createItemButton.tsx
@@ -1,6 +1,6 @@
 "use client"
 import CreateIcon from '@mui/icons-material/Create'
-import { useState, useRef, useContext } from 'react'
+import { useState, useRef, useContext, useEffect } from 'react'
 import { useCookies } from 'next-client-cookies';
 import { ModalWindow } from '../utils/modalWindow';
 import { useParams } from 'next/navigation';
@@ -18,6 +18,13 @@ export const CreateItemButton = () => {
   const { items, setItems } = useContext(ItemsContext)
   const params = useParams()
   const categoryId = params.categoryId;
+
+  useEffect(() => { // アイテム作成モーダルが開閉するとき、エラーメッセージをクリアする
+    if (wordErrorMessage || meaningErrorMessage) {
+      setWordErrorMessage("")
+      setMeaningErrorMessage("")
+    }
+  }, [modalIsOpen])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -46,6 +53,8 @@ export const CreateItemButton = () => {
         setMeaningErrorMessage(resJson.message)
       } else {
         setMeaningErrorMessage(resJson.message)
+        console.log("エラーが発生しました\n" + resJson.message)
+        alert("エラーが発生しました\n" + resJson.message)
       }
     }
   }
@@ -59,14 +68,50 @@ export const CreateItemButton = () => {
             modalClassName={UI_DATA.createItemModal.modalClassName}
             modalStyle={null}
           >
-            <form className='flex flex-col' onSubmit={handleSubmit}>
-              <label>言葉</label>
-              <input ref={wordRef}></input>
-              <span className='text-red-500'>{wordErrorMessage ?? ""}</span>
-              <label>意味</label>
-              <input ref={meaningRef}></input>
-              <span className='text-red-500'>{meaningErrorMessage ?? ""}</span>
-              <button type='submit'>保存</button>
+            <form onSubmit={handleSubmit} className="flex flex-col w-full h-full bg-slate-50 rounded-xl shadow-md">
+              <h1 className="text-center text-3xl text-blue-500 mt-3 mb-4">New Vocabulary</h1>
+              <div className="border-t flex h-10 py-1.5 mx-1">
+                <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+              </div>
+              <div className="border-t flex h-10 mx-1">
+                <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50  rounded-full"></span>
+                <label className="pl-2 pt-3">・言葉</label>
+              </div>
+              <div className="flex border-t h-10 mx-1">
+                <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+                <input ref={wordRef} type="text" placeholder="Apple" className="bg-slate-50 pl-2 pt-2 ml-4 w-full focus:outline-none"/>
+              </div>
+              { wordErrorMessage 
+              ? <div className="border-t flex h-10 mx-1">
+                  <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+                  <p className="pl-2 ml-4 pt-3 text-red-500">{wordErrorMessage}</p>
+                </div>
+              : <></>
+              }
+              <div className="border-t flex h-10 mx-1">
+                <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+                <label className="pl-2 pt-3">・意味</label>
+              </div>
+              <div className="border-t flex h-10 mx-1">
+                <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+                <input ref={meaningRef} type="text" placeholder="リンゴ" className="bg-slate-50 pl-2 pt-2 ml-4 w-full focus:outline-none"/>
+              </div>
+              { meaningErrorMessage 
+              ? <div className="border-t flex h-10 mx-1">
+                  <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+                  <p className="pl-2 ml-4 pt-3 text-red-500">{meaningErrorMessage}</p>
+                </div>
+              : <></>
+              }
+              <div className="border-t flex h-10 mx-1">
+                <span className="w-5 h-5 ml-2 my-auto shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+                <button type="submit" className="border-blue-500 border-2 text-blue-500 text-lg px-3 my-0.5 mx-auto hover:bg-blue-500 hover:text-white">保存</button>
+              </div>
+              <div className="border-t border-b flex h-10 mx-1">
+                <span className="w-5 h-5 ml-2 my-2 shadow-inner bg-slate-200 bg-opacity-50 rounded-full"></span>
+              </div>
+              <div className="h-14">
+              </div>
             </form>
           </ModalWindow>
         : <></>
