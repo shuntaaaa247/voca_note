2bee3231bade3b2588895940bc7261fd825da951
shuntaaaa247 <juntairenzu3@gmail.com>
2025-02-09 00:33:39 +0900

    カテゴリー名編集機能の追加（フロントエンド）

---
+++ frontend/src/components/leftBar/editCategoryNameForm.tsx
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";  
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCookies } from 'next-client-cookies';
+import { CategoryFormSchema, CategoryFormType} from "../utils/formType";
+import { useRouter } from "next/navigation";
+import { FormNoteLine } from "../utils/formNoteLine";
+
+export const EditCategoryNameForm = ({ 
+  categoryId,
+  categoryName,
+  setEditCategoryNameModalIsOpen,
+  setSelectionModalIsOpen
+ }: { 
+  categoryId: string,
+  categoryName: string,
+  setEditCategoryNameModalIsOpen: (isOpen: boolean) => void,
+  setSelectionModalIsOpen: (isOpen: boolean) => void
+}) => {
+  const router = useRouter();
+  const cookies = useCookies();
+  const token = cookies.get("token");
+  const { register, handleSubmit, formState: { errors }, setFocus } = useForm<CategoryFormType>({
+    resolver: zodResolver(CategoryFormSchema),
+  });
+
+  console.log(`errors: ${errors}`);
+
+  const onSubmit = async (data: CategoryFormType) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/${categoryId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": `Bearer ${token}`,
+      },
+      body: JSON.stringify({ newCategoryName: data.categoryName }),
+    });
+
+    const resJson = await res.json();
+
+    if (resJson.category) {
+      setEditCategoryNameModalIsOpen(false);
+      setSelectionModalIsOpen(false);
+      router.refresh();
+    } else {
+      alert(`エラーが発生しました: ${resJson.message}`);
+      console.log(resJson.message);
+    }
+  }
+  
+  useEffect(() => {
+    setFocus("categoryName");
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center w-full bg-slate-50 rounded-xl shadow-md">
+      <h1 className="text-center text-3xl text-green-600 mt-6 mb-4">Edit Category Name</h1>
+      <FormNoteLine>
+        <></>
+      </FormNoteLine>
+      <FormNoteLine>
+        <label className="pl-2 pt-3">・カテゴリー名</label>
+      </FormNoteLine>
+      <FormNoteLine>
+        <input type="text" {...register("categoryName")} defaultValue={categoryName} maxLength={200} minLength={1} className="w-full bg-slate-50 rounded-md pl-2 pt-2 ml-4 focus:outline-none"/>
+      </FormNoteLine>
+      {errors.categoryName?.message 
+      ? <FormNoteLine>
+          <p className="pl-2 ml-4 pt-3 text-red-500">{errors.categoryName?.message}</p>
+        </FormNoteLine>
+      : <></>
+      }
+      <FormNoteLine>
+        <button type="submit" className="border-green-600 border-2 text-green-600 text-lg px-3 my-0.5 mx-auto hover:bg-green-600 hover:text-white">編集を保存</button>
+      </FormNoteLine>
+      <FormNoteLine isDeepest={true}>
+        <></>
+      </FormNoteLine>
+      <div className="h-14">
+      </div>
+    </form>
+  )
+}
--- frontend/src/constants/uidata.ts
+++ frontend/src/constants/uidata.ts
@@ -3,21 +3,25 @@ export const UI_DATA = {
     screenClassName: "fixed flex justify-center items-center top-0 left-0 w-full h-full bg-slate-100 bg-opacity-80 z-10",
     modalClassName: "fixed w-[50%] bg-white  rounded-xl shadow-xl z-20"
   },
-  editItemModal: { // アイテム作成モーダル
+  editItemModal: { // アイテム編集モーダル
     screenClassName: "fixed flex justify-center items-center top-0 left-0 w-full h-full bg-slate-100 bg-opacity-80 z-10",
-    modalClassName: "fixed w-[50%] bg-white bg-opacity-50 rounded-xl shadow-xl z-20"
+    modalClassName: "fixed w-[50%] bg-white rounded-xl shadow-xl z-20"
   },
   createCategoryModal: { // カテゴリ作成モーダル
     screenClassName: "fixed top-0 left-0 w-full h-full bg-slate-100 bg-opacity-80 z-10",
     modalClassName: "fixed top-[20%] left-[20%] w-[60%] h-[60%] bg-white rounded-xl shadow-xl z-20"
   },
   selectionModalWidth: 120, // アイテムの[削除、編集]選択モーダルの横幅
-  selectionModal: {
+  selectionModal: { // アイテムの[削除、編集]選択モーダル
     screenClassName: "fixed top-0 left-0 w-full h-full z-10",
     modalClassName: "fixed w-[120px] h-1/12 bg-white rounded-xl shadow-xl z-20"
   },
-  confirmDeleteModal: {
+  confirmDeleteModal: { // アイテムの[削除]確認モーダル
     screenClassName: "fixed top-0 left-0 w-full h-full z-10",
     modalClassName: "fixed h-1/12 bg-white rounded-xl shadow-xl z-20"
+  },
+  editCategoryNameModal: { // カテゴリ名編集モーダル
+    screenClassName: "fixed flex justify-center items-center top-0 left-0 w-full h-full bg-slate-100 bg-opacity-80 z-10",
+    modalClassName: "fixed w-[50%] bg-white rounded-xl shadow-xl z-20"
   }
 }
