12eb53c9644ffc68bb0e8561511447fe18fa2089
忍足俊太 <oshitari_syunta@comsq.com>
2025-03-13 00:38:00 +0900

    ファイル名をTestNote.tsx→Note.tsxに変更

--- frontend/src/components/note/note.tsx
+++ frontend/src/components/note/note.tsx
@@ -1,183 +1,134 @@
 "use client"
-import { useState, useEffect } from "react";
-import InfiniteScroll from "react-infinite-scroller";
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect, useRef, useState, createContext } from "react"
+import useSWR from 'swr';
 import { useCookies } from 'next-client-cookies';
-import type { Item as ItemType } from "../../../../backend/generated/zod"
-import { Item } from "@/components/note/Item"
-import { CreateItemButton } from "@/components/note/CreateItemButton";
+import CircularProgress from '@mui/material/CircularProgress';
+import { Item as ItemType } from "../../../../backend/generated/zod"
+import { Item } from "./Item"
+import { CreateItemButton } from "./CreateItemButton"
 
-const categoryId: string = "45f6c41d-c78b-4cbf-88f9-1dbe16bd50b7"
+export const ItemsContext = createContext({} as {
+  items: ItemType[] | undefined,
+  setItems: React.Dispatch<React.SetStateAction<ItemType[] | undefined>>
+})
 
 export const Note = () => {
-  const [items, setItems] = useState<ItemType[] | null>()
+  const [items, setItems] = useState<ItemType[]>()
   const [hasMore, setHasMore] = useState<boolean>(true)
-  const cookies = useCookies();
-  const token = cookies.get("token");
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [fetchItemsError, setFetchItemsError] = useState<boolean>(false)
+  const loadingRef = useRef<HTMLDivElement>(null)
+  const cookies = useCookies()
+  const token = cookies.get("token")
+  const params = useParams()
+  const categoryId = params.categoryId;
+  const router = useRouter()
+  const currentCategoryFetcher = (url: string) => fetch(url, {
+    method: "GET",
+    headers: {
+      "authorization": `Bearer ${token}`
+    }
+  }).then(res => res.json())
 
-  const loadItems = async () => {
-    console.log("items => ", items)
-    let url: string;
-    if (items?.at(-1)?.createdAt) {
-      url = `${process.env.NEXT_PUBLIC_API_URL}/categories/45f6c41d-c78b-4cbf-88f9-1dbe16bd50b7/items?cursor=${items?.at(-1)?.createdAt}&limit=4`
-    } else {
-      url = `${process.env.NEXT_PUBLIC_API_URL}/categories/45f6c41d-c78b-4cbf-88f9-1dbe16bd50b7/items?limit=4`
+  const { data: categoryData, error: fetchCategoryError, isLoading: _isLoading } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/categories/${categoryId}`, currentCategoryFetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false
+  })
+
+  if (fetchCategoryError) {
+    // alert("エラーが発生しました:\n" + categoryError);
+
+    if (fetchCategoryError.status === 401) {
+      router.push("/login")
+      return;
     }
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        "authorization": `Bearer ${token}`
-      }
-    })
-    const resJson = await res.json()
-    const loadedItems: ItemType[] | null = resJson.items
-    console.log(loadedItems)
-    if (items) {
-      if (loadedItems?.length && loadedItems.length > 0) {
-        setItems([...items, ...loadedItems]);
-      } else {
-        setHasMore(false);
-      }
-    } else {
-      setItems(loadedItems)
-    }
+
+    alert("エラーが発生しました\nfetchCategoryError: " + fetchCategoryError);
+    console.log("エラーが発生しました\n", fetchCategoryError);
   }
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(async ([entry]) => { // コールバック関数([entry]) => {...}の引数[entry]は、IntersectionObserver.observe()で渡される引数を配列に格納したもの。すなわち[entry]なので、entryは配列の最初の要素。
+      if (entry.isIntersecting && hasMore) { // entryはIntersectionObserver.observe()で渡される最初の引数。（observe()は何回でも呼べて、監視対象を複数指定できるが、entryは最初に指定した監視対象ということ）
+        setIsLoading(true)
+        let url: string
+        if (!items) {
+          url = `${process.env.NEXT_PUBLIC_API_URL}/categories/${categoryId}/items?limit=10`
+        } else {
+          url = `${process.env.NEXT_PUBLIC_API_URL}/categories/${categoryId}/items?cursor=${items.at(-1)?.createdAt}&limit=10`
+        }
+        try {
+          const res = await fetch(url, { // 本当はuseEffect内でのfetchはよくない
+            method: "GET",
+            headers: {
+            "authorization": `Bearer ${token}`
+            }
+          });
 
-  return(
-    <div className="basis-5/6 flex justify-start my-5 py-20 mr-3 rounded-3xl shadow-xl overflow-auto bg-slate-50">
-      <div className="basis-full ">
-        <h2 className="pb-3 ml-7 mr-2 text-4xl text-slate-700 font-medium border-b-2">IT用語</h2>
-        <InfiniteScroll
-          key="infinite-scroll" // ここにもkeyをつける
-          loadMore={loadItems}
-          hasMore={hasMore}
-          loader={<p key="loader">ロード中</p>} // ここにもkeyをつける
-          initialLoad={true}
-          useWindow={false}
-          >
-          <ul className="my-4 mx-2">
-          {items?.map((item: ItemType) => {
-            return (
-              <Item id={item.id} word={item.word} meaning={item.meaning} categoryId={item.categoryId} createdAt={item.createdAt} updatedAt={item.updatedAt} key={item.id}/>
-            )
-          })}
-          </ul>
-        </InfiniteScroll>
-        {JSON.stringify(items)}
-        <CreateItemButton />
-      </div>
-    </div>
-  )
-}
+          if (res.status === 401) {
+            // alert("トークンが無効です。\n" + res.statusText);
+            router.push("/login")
+            return;
+          } 
+          
+          if (!res.ok) {
+            throw new Error("res.statusText: " + res.statusText + "\nres.status: " + res.status);
+          }
+          const resJson = await res.json();
+          const newItems: ItemType[] = resJson.items
+          console.log("newItems => ", newItems)
+          if (newItems.length > 0) {
+            setItems([...items ?? [] , ...newItems ?? []])
+            console.log("setItemsを実行しました")
+          } else {
+            setHasMore(false)
+            console.log("setHasMore(false)を実行しました")
+          }
+          // setIsLoading(false)
+        } catch (error) {
+          alert("エラーが発生しました\n");
+          console.log("エラーが発生しました\n", error);
+          setFetchItemsError(true)
+        }
+        setIsLoading(false)
+      }
+    })
 
-const BlankList = () => {
+    if (loadingRef.current) {
+      observer.observe(loadingRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [items?.length ?? null, hasMore])
+
   return (
-    <>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
+    <ItemsContext.Provider value={{ items, setItems }}>
+      <div className="basis-5/6 flex justify-start my-5 py-20 mr-3 rounded-3xl shadow-xl overflow-auto bg-slate-50">
+        <div className="basis-full ">
+          { _isLoading
+            ? <CircularProgress />
+            : 
+              fetchCategoryError
+                ? <p className="pb-3 ml-7 mr-2 text-xl text-red-500 font-medium border-b-2">エラー：カテゴリーを取得できませんでした。</p>
+                : <h2 className="pb-3 ml-7 mr-2 text-4xl text-slate-700 font-medium border-b-2">{ categoryData?.category?.categoryName ?? "エラー：カテゴリーを取得できませんでした。" }</h2>
+          }
+          { fetchItemsError
+          ? <p className="ml-7 text-red-500 font-medium">エラー：アイテムを取得できませんでした。</p>
+          : 
+            <ul className="my-4 mx-2">
+              {items?.map((item: ItemType) => {
+                return (
+                  <Item {...item} key={item.id}/>
+                )
+              })}
+            </ul>
+          }
+        
+          <CreateItemButton />
+          <div ref={loadingRef} className='flex justify-center'>{ isLoading ? <CircularProgress /> : <></>}</div>
         </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-      <li className="pl-2 h-[70px]">
-        <div className="flex justify-start">
-          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
-          <div className="w-full border-b">
-          </div>
-        </div>
-      </li>
-    </>        
+      </div>
+    </ItemsContext.Provider>
   )
 }
