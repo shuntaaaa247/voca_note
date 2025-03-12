b0a8a580ed282be8a0d1a67b9248b28faa7088ea
忍足俊太 <oshitari_syunta@comsq.com>
2025-03-12 10:23:07 +0900

    試験的に

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app"
-import "dotenv/config"
-
-const port: number = Number(process.env.PORT) || 5000;
-
-app.listen(port, () => {
-  console.log(`backend is running on ${port}`)
-})
--- frontend/src/__tests__/__mocks__/cookies.ts
+++
@@ -1,5 +0,0 @@
-export const mockCookieStore = new Map<string, string>()
-export const mockUseCookies = {
-  set: jest.fn().mockImplementation((key: string, value: string) => mockCookieStore.set(key, value)),
-  get: jest.fn().mockImplementation((key: string) => mockCookieStore.get(key)),
-}
