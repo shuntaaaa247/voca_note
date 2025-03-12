7b2d96f352cfa0e9a7d2750fef58f0e965dbe88b
shuntaaaa247 <juntairenzu3@gmail.com>
2025-03-12 22:43:10 +0900

    コンポーネントファイル名の先頭を大文字に

---
+++ frontend/src/components/note/CreateItemButton.tsx
@@ -0,0 +1,31 @@
+"use client"
+import CreateIcon from '@mui/icons-material/Create'
+import { useState, useRef, useContext, useEffect } from 'react'
+import { ModalWindow } from '../utils/ModalWindow'; 
+import { CreateItemForm } from './CreateItemForm';
+import { UI_DATA } from '../../constants/uidata';
+
+export const CreateItemButton = () => {
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+
+  return(
+    <>
+      { modalIsOpen 
+        ? <ModalWindow 
+            setModalIsOpen={setModalIsOpen} 
+            screenClassName={UI_DATA.createItemModal.screenClassName} 
+            modalClassName={UI_DATA.createItemModal.modalClassName}
+            modalStyle={null}
+          >
+            <CreateItemForm setModalIsOpen={setModalIsOpen}/>
+          </ModalWindow>
+        : <></>
+      }
+      <div className="fixed bottom-10 right-10">
+        <button className='bg-blue-500 p-3 rounded-full hover:bg-blue-400' onClick={() => setModalIsOpen(true)}>
+          <CreateIcon data-testid="CreateIcon" sx={{color: 'white', fontSize: 50}}/>
+        </button>
+      </div>
+    </>
+  )
+}
---
+++ frontend/src/components/utils/ModalWindow.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useEffect } from "react"
+
+type ModalStyle = {
+  top: string
+  left: string
+}
+
+type Props = {
+  setModalIsOpen: (value: boolean) => void
+  screenClassName: string
+  modalClassName: string
+  modalStyle?: ModalStyle | null
+  children: ReactNode
+}
+
+export const ModalWindow = ({ setModalIsOpen, screenClassName, modalClassName, modalStyle, children }: Props) => {
+  const stopScrollingBackContent = () => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    }
+  };
+
+  useEffect(stopScrollingBackContent, []);
+  return(
+    <div data-testid="modalWindow" className={screenClassName} onClick={() => setModalIsOpen(false)}>
+      <div className={modalClassName} style={modalStyle ?? {}} onClick={(e) => e.stopPropagation()}>
+        <div className='flex justify-center '>
+          {children}
+        </div>
+      </div> 
+    </div>
+  )
+}
