792b9ba78fc39cf53d7e72ff113c5199b9548160
忍足俊太 <oshitari_syunta@comsq.com>
2025-03-13 00:34:36 +0900

    デザインの変更とファイル名の変更

---
+++ frontend/src/components/note/HomeNote.tsx
@@ -0,0 +1,126 @@
+export const HomeNote = () => {
+  return (
+    <div className="basis-5/6 flex justify-start my-5 py-20 mr-3 rounded-3xl shadow-xl overflow-auto bg-slate-50">
+      <div className="basis-full">
+        <h2 className="pb-3 ml-7 mr-2 text-4xl text-slate-700 font-medium border-b-2">VOCA NOTE へようこそ</h2>
+        <IntroductionList />
+      </div>
+    </div>
+  )
+}
+
+const IntroductionList = () => {
+  return (
+    <>
+      <ul className="my-4 mx-2">
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b flex items-center">
+            <p className="text-2xl font-medium bg-red-50">カテゴリーを作成してみましょう！</p>
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b flex items-center">
+            <p className="text-2xl font-medium bg-green-50">カテゴリーに覚えたい用語を追加してみましょう！</p>
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      <li className="pl-2 h-[70px]">
+        <div className="flex justify-start">
+          <span className="mr-5 w-7 h-7 rounded-full bg-slate-200 shadow-inner my-5"></span>
+          <div className="w-full border-b">
+          </div>
+        </div>
+      </li>
+      </ul>
+    </>        
+  )
+}
